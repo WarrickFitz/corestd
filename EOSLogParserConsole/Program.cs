230714d83107063d1fc4b420393431306cdb2304
Warrick FitzGerald <warrick@eosnewyork.io>
2018-07-21 13:33:13 -0400

    Added websocket input

--- EOSLogParserConsole/Program.cs
+++ EOSLogParserConsole/Program.cs
@@ -3,37 +3,31 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Threading;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Concurrent;
 
 namespace EOSLogParserConsole
 {
 
     public class producerState
     {
-        Dictionary<string, producerStats> producers { get; set; }
+        Dictionary<string, producerStats> producers { get; set; } = new Dictionary<string, producerStats>();
 
-        public producerState()
+        public void registerBlockProduction(lineParser lineObj)
         {
-            producers = new Dictionary<string, producerStats>();
-        }
-
-        public void registerBlockProduction(string producer, Int64 block)
-        {
-            var exists = producers.ContainsKey(producer);
-            if(exists)
+            var exists = producers.ContainsKey(lineObj.part_producer);
+            if(!exists)
             {
-                
+                producers.Add(lineObj.part_producer, new producerStats());
             }
-            else
-            {
-                var stats = new producerStats();
-                producers.Add(producer, stats);
-            }
-            producers[producer].blockCount++;
+            producers[lineObj.part_producer].registerBlockProduction(lineObj);
         }
 
         public void print(String producer)
         {
-            Console.WriteLine(string.Format("{0}\t{1}", producer, producers[producer].blockCount));
+            Console.WriteLine(string.Format("{0}\t{1}", producer, producers[producer].hightestBlock));
         }
 
 
@@ -41,105 +35,106 @@ namespace EOSLogParserConsole
 
     public class producerStats
     {
+        public Int64 hightestBlock { get; set; } = 0;
+        Dictionary<DateTime, slotStats> slots { get; set; } = new Dictionary<DateTime, slotStats>();
+        public void registerBlockProduction(lineParser lineObj)
+        {
+            if (lineObj.blockNumber > hightestBlock)
+            {
+                if((lineObj.blockNumber - hightestBlock) > 12)
+                {
+                    slots.Add(lineObj.signDateTime, new slotStats());
+                }
+                hightestBlock = lineObj.blockNumber;
+
+            }
+                
+        }
+    }
+
+    public class slotStats
+    {
         public int blockCount { get; set; }
+        Dictionary<DateTime, producerStats> producers { get; set; } = new Dictionary<DateTime, producerStats>();
     }
-
-    public class lineParser {
-
-
-        public string part_dateTime { get; set; }
-        public DateTime dateTime { get
-            {
-                return DateTime.SpecifyKind((DateTime.Parse(part_dateTime)),DateTimeKind.Utc);
-            }
-        }
-        public string part_thread { get; set; }
-        public string part_module { get; set; }
-        public string part_action { get; set; }
-        public string part_blockInfo { get; set; }
-        public string part_blockNumber { get; set; }
-        public Int64 blockNumber { get
-            {
-                return Int64.Parse(part_blockNumber);
-            }
-        }
-        public string part_signDateTime { get; set; }
-        public string part_producer { get; set; }
-
-        public lineParser(string ln)
-        {
-            part_dateTime = ln.Substring(0, 23);
-            part_thread = ln.Substring(24, 10);
-            part_module = ln.Substring(35, 29);
-            part_action = ln.Substring(65, 21);
-            part_blockInfo = ln.Substring(88, 34);
-
-            var remainder = ln.Substring(124, ln.Length - 124);
-            var lineParts = remainder.Split("@");
-
-            part_blockNumber = lineParts[0].Trim();
-
-            var signParts = lineParts[1].Split("[");
-
-
-            var signByParts = signParts[0].Trim().Split(" ");
-            part_signDateTime = signByParts[0];
-            part_producer = signByParts[signByParts.Length - 1];
-
-        }
-
-        public void print()
-        {
-            
-
-            Console.WriteLine(string.Format("{0}\t{1}\t{2}", dateTime.ToLocalTime(), part_producer, blockNumber));
-            /*
-            Console.WriteLine(dateTime.ToLocalTime());
-            Console.WriteLine(part_dateTime);
-            Console.WriteLine(part_thread);
-            Console.WriteLine(part_module);
-            Console.WriteLine(part_action);
-            Console.WriteLine(part_blockInfo);
-            Console.WriteLine(blockNumber);
-            Console.WriteLine(part_signDateTime);
-            Console.WriteLine(part_producer);
-            */
-        }
-    }
-
-
-
 
 
     class Program
     {
         static producerState state = new producerState();
+        public static ConcurrentQueue<String> logQ = null;
+
+
 
         static void Main(string[] args)
         {
+            LogsFromWebsocket logSource = new LogsFromWebsocket("ws://badger.eosdocs.io:8080/");
+            logSource.start();
+
+
+            Thread thread = new Thread(new ThreadStart(WorkThreadFunction));
+            thread.Start();
+            logQ = LogsFromWebsocket.logQ;
+
+            Console.ReadLine();
+
+
             /*
             string ln = @"2018-07-20T18:19:42.500 thread-0   producer_plugin.cpp:1194      produce_block        ] Produced block 000047b9f6c393b2... #18361 @ 2018-07-20T18:19:42.500 signed by bp2 [trxs: 0, lib: 18347, confirmed: 0]";
             exec(ln);
             exec(ln);
-            */
+
             Console.ReadLine();
             string s;
             while ((s = Console.ReadLine()) != null)
             {
                 exec(s);
             }
+            */
 
+    }
 
-        }
-
-        static void exec(string s)
+        static void processLine(string s)
         {
             //Console.WriteLine(s);
-            lineParser line = new lineParser(s);
-            state.registerBlockProduction(line.part_producer, line.blockNumber);
-            line.print();
-            state.print(line.part_producer);
-            
+            lineParser lineObj = new lineParser(s);
+            state.registerBlockProduction(lineObj);
+            lineObj.print();
+            //state.print(lineObj.part_producer);           
         }
+
+
+
+        public static void WorkThreadFunction()
+        {
+            try
+            {
+                while(1<2)
+                {
+                    
+                    // I'm not sure why, but if you try to access the queue to quickly the application hangs. 
+                    Thread.Sleep(1000);
+                    while(logQ.Count > 0)
+                    {
+
+                        String result = String.Empty;
+                        var qItem = logQ.TryDequeue(out result);
+                        if(result.Contains("on_incoming_block") || result.Contains("produce_block"))
+                        {
+                            processLine(result);
+                        }
+                        
+                    }                   
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+
+
     }
 }
---
+++ EOSLogParserConsole/lineParser.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EOSLogParserConsole
+{
+    public class lineParser
+    {
+
+        /*
+        public string part_dateTime { get; set; }
+        public DateTime dateTime { get
+            {
+                return DateTime.SpecifyKind((DateTime.Parse(part_dateTime)),DateTimeKind.Utc);
+            }
+        }
+        */
+        //public string part_thread { get; set; }
+        //public string part_module { get; set; }
+        public string part_action { get; set; }
+        public string part_blockInfo { get; set; }
+        public string part_blockNumber { get; set; }
+        public Int64 blockNumber
+        {
+            get
+            {
+                return Int64.Parse(part_blockNumber);
+            }
+        }
+        public string part_signDateTime { get; set; }
+        public DateTime signDateTime
+        {
+            get
+            {
+                return DateTime.SpecifyKind((DateTime.Parse(part_signDateTime)), DateTimeKind.Utc);
+            }
+        }
+        public string part_producer { get; set; }
+
+        public lineParser(string ln)
+        {
+            var lineParts = ln.Split("@");
+            var prefixParts = lineParts[0].Trim().Split(" ");
+
+            part_blockNumber = prefixParts[prefixParts.Length - 1].Trim().Replace("#", "");
+
+            var signParts = lineParts[1].Split("[");
+            var signByParts = signParts[0].Trim().Split(" ");
+
+            part_signDateTime = signByParts[0];
+            part_producer = signByParts[signByParts.Length - 1];
+
+        }
+
+        public void print()
+        {
+            Console.WriteLine(string.Format("{0}\t{1}\t{2}", signDateTime.ToLocalTime(), part_producer, blockNumber));
+        }
+    }
+}
